088d5d49e8422235b5aac28f352076ba14b7ef1d
abdualim <08082004abdualim@gmail.com>
2023-05-06 17:36:06 +0500

    Update project

---
+++ target/generated-sources/annotations/com/company/LibraryProject/service/mapper/BookMapperImpl.java
@@ -0,0 +1,63 @@
+package com.company.LibraryProject.service.mapper;
+
+import com.company.LibraryProject.dto.BooksDto;
+import com.company.LibraryProject.model.Book;
+import java.util.stream.Collectors;
+import javax.annotation.processing.Generated;
+import org.springframework.stereotype.Component;
+
+@Generated(
+    value = "org.mapstruct.ap.MappingProcessor",
+    date = "2023-05-06T17:25:21+0500",
+    comments = "version: 1.5.3.Final, compiler: javac, environment: Java 20.0.1 (Oracle Corporation)"
+)
+@Component
+public class BookMapperImpl extends BookMapper {
+
+    @Override
+    public Book toEntity(BooksDto dto) {
+        if ( dto == null ) {
+            return null;
+        }
+
+        Book book = new Book();
+
+        book.setBookId( dto.getBookId() );
+        book.setName( dto.getName() );
+        book.setPrice( dto.getPrice() );
+        book.setPage( dto.getPage() );
+        book.setAmount( dto.getAmount() );
+        book.setPublisherAt( dto.getPublisherAt() );
+        book.setCreatedAt( dto.getCreatedAt() );
+        book.setUpdatedAt( dto.getUpdatedAt() );
+        book.setDeletedAt( dto.getDeletedAt() );
+
+        return book;
+    }
+
+    @Override
+    public BooksDto toDto(Book book) {
+        if ( book == null ) {
+            return null;
+        }
+
+        BooksDto booksDto = new BooksDto();
+
+        booksDto.setBookId( book.getBookId() );
+        booksDto.setName( book.getName() );
+        booksDto.setPrice( book.getPrice() );
+        booksDto.setPage( book.getPage() );
+        booksDto.setAmount( book.getAmount() );
+        booksDto.setPublisherAt( book.getPublisherAt() );
+        booksDto.setCreatedAt( book.getCreatedAt() );
+        booksDto.setUpdatedAt( book.getUpdatedAt() );
+        booksDto.setDeletedAt( book.getDeletedAt() );
+
+        booksDto.setAuthorsDto( book.getAuthors().stream().map(authorsMapper::toDtoNotAuthorId).collect(Collectors.toSet()) );
+        booksDto.setImagesDto( book.getImages().stream().map(imagesMapper::toDtoNotBookId).collect(Collectors.toSet()) );
+        booksDto.setGoalsDto( goalsMapper.toDtoNotBookId(book.getGoals()) );
+        booksDto.setPublisherDto( publisherMapper.toDtoNotBookId(book.getPublisher()) );
+
+        return booksDto;
+    }
+}
---
+++ target/generated-sources/annotations/com/company/LibraryProject/service/mapper/GoalsMapperImpl.java
@@ -0,0 +1,48 @@
+package com.company.LibraryProject.service.mapper;
+
+import com.company.LibraryProject.dto.GoalsDto;
+import com.company.LibraryProject.model.Goals;
+import javax.annotation.processing.Generated;
+import org.springframework.stereotype.Component;
+
+@Generated(
+    value = "org.mapstruct.ap.MappingProcessor",
+    date = "2023-05-06T17:25:21+0500",
+    comments = "version: 1.5.3.Final, compiler: javac, environment: Java 20.0.1 (Oracle Corporation)"
+)
+@Component
+public class GoalsMapperImpl extends GoalsMapper {
+
+    @Override
+    public Goals toEntity(GoalsDto goalsDto) {
+        if ( goalsDto == null ) {
+            return null;
+        }
+
+        Goals goals = new Goals();
+
+        goals.setGoalsId( goalsDto.getGoalsId() );
+        goals.setLanguage( goalsDto.getLanguage() );
+        goals.setDescription( goalsDto.getDescription() );
+
+        return goals;
+    }
+
+    @Override
+    public GoalsDto toDto(Goals goals) {
+        if ( goals == null ) {
+            return null;
+        }
+
+        GoalsDto goalsDto = new GoalsDto();
+
+        goalsDto.setGoalsId( goals.getGoalsId() );
+        goalsDto.setLanguage( goals.getLanguage() );
+        goalsDto.setDescription( goals.getDescription() );
+        goalsDto.setCreatedAt( goals.getCreatedAt() );
+        goalsDto.setUpdatedAt( goals.getUpdatedAt() );
+        goalsDto.setDeletedAt( goals.getDeletedAt() );
+
+        return goalsDto;
+    }
+}
